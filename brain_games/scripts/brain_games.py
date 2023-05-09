f28bcda69659886aa9e41569e5cecd30b9bf5d05
Loginov <itsmeegorloginov@gmail.com>
2023-05-09 20:50:29 +0300

    transfered funktion to cli.py and imported it to brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import prompt
 
+from ..cli import welcome_user
 def main():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-
+    welcome_user()
 if __name__ == '__main__':
     main()
